8d1a8ac0889c498a463b04627e4a542730b7409e
Ruslan <ruslan2098007@gmail.com>
2023-07-02 20:38:49 +0300

    add new gulp

--- src/js/script.js
+++ src/js/script.js
@@ -1,3 +1,39 @@
 "use strict"
 
 const swiper = new Swiper('.swiper');
+
+// Перевірка на сенсорні екрани, і додаємо класс для body.
+const isMobile = {
+    Android: function() {
+        return navigator.userAgent.match(/Android/i);
+    },
+    BlackBorry: function() {
+        return navigator.userAgent.match(/BlackBorry/i);
+    },
+    IOS: function() {
+        return navigator.userAgent.match(/iPhone|iPad|iPod/i);
+    },
+    Opera: function() {
+        return navigator.userAgent.match(/Opera Mini/i);
+    },
+    Windows: function() {
+        return navigator.userAgent.match(/IEMobile/i);
+    },
+    any: function() {
+        return (
+            isMobile.Android() ||
+            isMobile.BlackBorry() ||
+            isMobile.IOS() ||
+            isMobile.Opera() ||
+            isMobile.Windows()
+        );
+    }
+};
+
+if (isMobile.any()){
+    document.body.classList.add('_touch');
+} else {
+    document.body.classList.add('_pc');
+}
+
+let events = document.body.classList.contains('_touch') ? 'click' : 'mousemove';
